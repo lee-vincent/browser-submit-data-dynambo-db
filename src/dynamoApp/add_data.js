7c98b214ee270c2e94cf0b0c077882d1f65aa5a7
lee-vincent <vinnie@vinnielee.io>
2023-10-22 04:48:24 -0400

    removed sns functionality

--- src/dynamoApp/add_data.js
+++ src/dynamoApp/add_data.js
@@ -24,8 +24,6 @@ node add_data.js
 // snippet-start:[s3.JavaScript.cross-service.addDataV3.config]
 // Import required AWS SDK clients and commands for Node.js
 import { PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { PublishCommand } from "@aws-sdk/client-sns";
-import { snsClient } from "../libs/snsClient.js";
 import { dynamoClient } from "../libs/dynamoClient.js";
 
 // snippet-end:[s3.JavaScript.cross-service.addDataV3.config]
@@ -58,23 +56,6 @@ export const submitData = async () => {
       //Upload the item to the table
       await dynamoClient.send(new PutItemCommand(params));
       alert("Data added to table.");
-      try {
-        // Create the message parameters object.
-        const messageParams = {
-          Message: "A new item with ID value was added to the DynamoDB",
-          PhoneNumber: "+15085057577", //PHONE_NUMBER, in the E.164 phone number structure.
-          // For example, ak standard local formatted number, such as (415) 555-2671, is +14155552671 in E.164
-          // format, where '1' in the country code.
-        };
-        // Send the SNS message
-        const data = await snsClient.send(new PublishCommand(messageParams));
-        console.log(
-          "Success, message published. MessageID is " + data.MessageId,
-        );
-      } catch (err) {
-        // Display error message if error is not sent
-        console.error(err, err.stack);
-      }
     } catch (err) {
       // Display error message if item is no added to table
       console.error(
